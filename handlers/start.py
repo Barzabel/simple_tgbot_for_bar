d014e8b9625b4b286ba13c814065212c8ea54379
Barzabel <Kochenko1995@mail.ru>
2024-09-07 11:43:29 +0300

    добавить простую клавиатуру

--- handlers/start.py
+++ handlers/start.py
@@ -2,16 +2,25 @@ from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram.types import FSInputFile
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+button_hi = KeyboardButton('menu')
+
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
+
 
 start_router = Router()
 
 @start_router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer('hi')
+    await message.answer('hi', reply_markup=greet_kb)
 
 @start_router.message(Command('menu'))
 async def send_menu(message: Message):
     menu = FSInputFile('data/menu.pdf')
-    await message.reply_document(menu)
+    await message.reply_document(menu, reply_markup=greet_kb)
     
 
